3199b13545d0b945a72533953f9945820409bc09
Riyazkhan-Nazeerbasha <nriyazkhan0518@gmail.com>
2024-09-13 16:43:15 +0530

    First Commit

---
+++ src/main/java/com/automationpro/AutoMationBuildingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.automationpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMationBuildingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMationBuildingProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/automationpro/controller/AutoMationBuildingController.java
@@ -0,0 +1,13 @@
+package com.automationpro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutoMationBuildingController {
+	@GetMapping(value="/get")
+	public String get() {
+		return "Hi da Velu mavaneyyyy";
+	}
+
+}
